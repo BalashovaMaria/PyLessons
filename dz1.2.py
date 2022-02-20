fbcef5c30a96214117b80dfcf0f7d8856e9ad42c
BalashovaMaria <99802613+BalashovaMaria@users.noreply.github.com>
2022-02-20 17:22:34 +0300

    Add files via upload

---
+++ dz1.2.py
@@ -0,0 +1,7 @@
+firstname = input('ваше имя строчными буквами:')
+surname = input('ваша фамилия строчными буквами:')
+firstname = firstname.title()
+surname = surname.title()
+name = firstname + ' ' + surname
+print(name)
+
